c2363960fd2ae588b0069c6977fa4abd7e9614bf
Emma Zhang <emmazhang211@hotmail.com>
2015-10-31 16:18:01 -0700

    events now use parse data, order by dueDate, example see group "UITesting"

--- Grouplus/app/components/Events.js
+++ Grouplus/app/components/Events.js
@@ -2,12 +2,17 @@
  * Display uploaded photos and UI for adding new ones.
  */
 var React = require('react-native');
+var ParseReact = require('parse-react/react-native');
+var ParseComponent = ParseReact.Component(React);
+var Parse = require('parse/react-native');
+Parse.initialize("***REMOVED***", "***REMOVED***");
 
 var Modal = require('react-native-modalbox');
 var EventItem = require('./EventItem');
 var Separator = require('./helpers/Separator');
 var EventAdd = require('./EventAdd');
 var mockdata = require('../utils/MockData');
+
 
 var {
   StyleSheet,
@@ -32,13 +37,19 @@ var styles = StyleSheet.create({
   },
 });
 
-class Events extends React.Component{
+class Events extends ParseComponent{
   constructor(props){
     super(props);
     this.ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2});
+    /*
     this.state = {
       dataSource: this.ds.cloneWithRows(this.props.events),
-    };
+    };*/
+  }
+  observe(props, state) {
+    return {
+      events: (new Parse.Query('Event')).equalTo('groupId', this.props.group.objectId).ascending('dueDate'),
+    }
   }
 
   onPressNewEvent() {
@@ -69,7 +80,7 @@ class Events extends React.Component{
     return (
       <View style={basicStyles.flex1}>
         <ListView 
-          dataSource={this.state.dataSource}
+          dataSource={this.ds.cloneWithRows(this.data.events)}
           renderRow={this._renderRow.bind(this)} 
           renderFooter={this._renderFooter.bind(this)}
           contentInset={{top:64}}
