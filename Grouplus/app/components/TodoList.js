cb59fb440ff0bf579ccb33220a180c91b0507fbc
Emma Zhang <emmazhang211@hotmail.com>
2015-11-01 16:25:42 -0800

    delete and check done todo item is done, commented out some code in people.js, need to UI to reflect to who has Done, and people img_url

--- Grouplus/app/components/TodoList.js
+++ Grouplus/app/components/TodoList.js
@@ -1,95 +1,140 @@
-/**
- * Display a list of todo items
- *
- * TODOs: make it swipable 
- */
+  /**
+   * Display a list of todo items
+   *
+   * TODOs: make it swipable 
+   */
 
-var React = require('react-native');
-var ParseReact = require('parse-react/react-native');
-var ParseComponent = ParseReact.Component(React);
-var Parse = require('parse/react-native');
-Parse.initialize("***REMOVED***", "***REMOVED***");
+  var React = require('react-native');
+  var ParseReact = require('parse-react/react-native');
+  var ParseComponent = ParseReact.Component(React);
+  var Parse = require('parse/react-native');
+  Parse.initialize("***REMOVED***", "***REMOVED***");
 
-var {
-  View,
-  ListView,
-  StyleSheet,
-  ScrollView,
-  TouchableHighlight,
-  Text,
-  NavigatorIOS,
-} = React;
+  var {
+    View,
+    ListView,
+    StyleSheet,
+    ScrollView,
+    TouchableHighlight,
+    Text,
+    NavigatorIOS,
+  } = React;
 
-var Modal = require('./helpers/Modal');
-var Separator = require('./helpers/Separator');
-var TodoItem = require('./TodoItem');
-var TodoAdd = require('./TodoAdd');
+  var Modal = require('./helpers/Modal');
+  var Separator = require('./helpers/Separator');
+  var Swipeout = require('./helpers/Swipeout');
+  var TodoItem = require('./TodoItem');
+  var TodoAdd = require('./TodoAdd');
 
-var basicStyles = require('./helpers/Styles');
-var styles = StyleSheet.create({
-});
+  var basicStyles = require('./helpers/Styles');
+  var styles = StyleSheet.create({
+  });
 
-class TodoList extends ParseComponent{
-  constructor(props){
-    super(props);
-    this.ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2});
-    
-    /*this.state = {
-      dataSource: this.ds.cloneWithRows(this.props.todos),
-    };*/
-  }
-  observe(props, state) {
-    return {
-      todos: (new Parse.Query('Todo')).equalTo('group', this.props.group.objectId),
+  class TodoList extends ParseComponent{
+    constructor(props){
+      super(props);
+      this.ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2});
     }
-  }
+    observe(props, state) {
+      return {
+        todos: (new Parse.Query('Todo')).equalTo('group', this.props.group.objectId),
+        user: Parse.User.current(), 
+      }
+    }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({dataSource: this.ds.cloneWithRows(/*nextProps.todos*/ this.data.todos)});
-  }
-  
-  onPressNewTodo() {
-    this.refs.addTodo.open();
-  }
+    componentWillReceiveProps(nextProps) {
+      this.setState({dataSource: this.ds.cloneWithRows(/*nextProps.todos*/ this.data.todos)});
+    }
+    
+    onPressNewTodo() {
+      this.refs.addTodo.open();
+    }
 
-  renderRow(rowData) {
-    return (
-      <View>
-        <TodoItem todo={rowData}/>
-        <Separator/>
-      </View>
-    );
-  }
+    onPressDelete(rowData){
+      alert(rowData);
+     var target = {
+    className: 'Todo',
+    objectId: rowData.objectId,
+    };
 
-  renderFooter() {
-      console.log("group : " + this.props.group);
-    return (
-      <TouchableHighlight style={basicStyles.button}  navigator={this.props.navigator}
-          group={this.props.group} onPress={() => this.onPressNewTodo()}>
-        <Text style={basicStyles.buttonText}>Add New Todo</Text>
-      </TouchableHighlight>
+  //ParseReact.Mutation.Destroy(target).dispatch();
+    }
+
+    renderRow(rowData) {
+
+      var deleteBtn = {
+      text: 'Delete', 
+      backgroundColor: '#ff0000',
+          onPress: function(){
+      var target = {
+        className: 'Todo',
+        objectId: rowData.objectId,
+       };
+       ParseReact.Mutation.Destroy(target).dispatch();
+    }
+  }; 
+
+      var checkFinishBtn = {
+      text: 'Done', 
+      backgroundColor:'#32cd32',
+
+   onPress: function(){
+         var target = {
+        className: 'Todo',
+        objectId: rowData.objectId,
+       };
+       var uid = Parse.User.current().id; 
+       var index = rowData.whoAreDone.indexOf(uid);
+       if(index <0){
+          rowData.whoAreDone.push(uid); 
+          ParseReact.Mutation.Set(target, { whoAreDone: rowData.whoAreDone}).dispatch();
+       }
+      }
+    }; 
+
+      var swipeBtn = [checkFinishBtn, deleteBtn];
+
+      return (
+        
+        <Swipeout backgroundColor={'#fff'} autoClose={true} right={swipeBtn}>
+        <View>
+          <TodoItem todo={rowData}/>
+          <Separator/>
+        </View>
+        </Swipeout>
+
+
       );
-  }
+    }
 
-  render(){
-    return (
-      <View style={basicStyles.flex1}>
-        <ListView
-          dataSource={this.ds.cloneWithRows(this.data.todos)}
-          renderRow={this.renderRow.bind(this)} 
-          renderFooter={this.renderFooter.bind(this)}
-          contentInset={{top:64, bottom: 50}}
-          automaticallyAdjustContentInsets={false}
-        />
-        <Modal ref={'addTodo'} component={TodoAdd} />
-      </View>
-    );
-  }
-};
+    renderFooter() {
+        console.log("group : " + this.props.group);
+      return (
+        <TouchableHighlight style={basicStyles.button}  navigator={this.props.navigator}
+            group={this.props.group} onPress={() => this.onPressNewTodo()}>
+          <Text style={basicStyles.buttonText}>Add New Todo</Text>
+        </TouchableHighlight>
+        );
+    }
 
-/*
-TodoList.propTypes = {
-  todos: React.PropTypes.array.isRequired,
-}*/
+    render(){
+      return (
+        <View style={basicStyles.flex1}>
+          <ListView
+            dataSource={this.ds.cloneWithRows(this.data.todos)}
+            renderRow={this.renderRow.bind(this)} 
+            renderFooter={this.renderFooter.bind(this)}
+            contentInset={{top:64, bottom: 50}}
+            automaticallyAdjustContentInsets={false}/>
+          <Modal ref={'addTodo'} component={TodoAdd} />
+        </View>
+      );
+    }
+  };
 
-module.exports = TodoList;
+  /*
+  TodoList.propTypes = {
+    todos: React.PropTypes.array.isRequired,
+  }*/
+
+  module.exports = TodoList;
--- Grouplus/app/components/helpers/Styles.js
+++ Grouplus/app/components/helpers/Styles.js
@@ -23,4 +23,45 @@ module.exports = StyleSheet.create({
   flex1: {
     flex: 1,
   },
+swipeout: {
+    backgroundColor: '#dbddde',
+    flex: 1,
+    overflow: 'hidden',
+  },
+  swipeoutBtnTouchable: {
+    flex: 1,
+  },
+  swipeoutBtn: {
+    alignItems: 'center',
+    backgroundColor: '#b6bec0',
+    flex: 1,
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+  swipeoutBtnText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  swipeoutBtns: {
+    bottom: 0,
+    flex: 1,
+    flexDirection: 'row',
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  swipeoutContent: {
+    flex: 1,
+  },
+  colorDelete: {
+    backgroundColor: '#fb3d38',
+  },
+  colorPrimary: {
+    backgroundColor: '#006fff'
+  },
+  colorSecondary: {
+    backgroundColor: '#fd9427'
+  },
+
+  
 });
