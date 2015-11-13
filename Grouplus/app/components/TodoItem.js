dbc09b8defd11c87b437171a061c54e3ffc328a2
Stephanie Choi <steph.choi92@gmail.com>
2015-11-12 16:23:44 -0800

    changed the color for TodoList

--- Grouplus/app/components/TodoItem.js
+++ Grouplus/app/components/TodoItem.js
@@ -26,6 +26,25 @@ var styles = StyleSheet.create({
     flex: 7,
     textAlign: 'left',
   },
+  titleCritical: {
+    fontSize: 22,
+    flex: 7,
+    textAlign: 'left',
+    color: 'red',
+    fontWeight: 'bold',
+  },
+  titleHigh: {
+    fontSize: 22,
+    flex: 7,
+    textAlign: 'left',
+    color: 'orange',
+  },
+  titleMedium: {
+    fontSize: 22,
+    flex: 7,
+    textAlign: 'left',
+    color: 'blue',
+  },
   info: {
     flex: 2,
     flexDirection: 'column',
@@ -33,6 +52,10 @@ var styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   dueDate: {
+    fontSize: 14,
+    color: 'black',
+  },
+  dueDatePast: {
     fontSize: 14,
     color: 'red',
   },
@@ -47,12 +70,27 @@ class TodoItem extends React.Component{
     var todo = this.props.todo;
     var dueDate = todo.dueDate ? moment(todo.dueDate).format('MMM D') : '';
     var individual = todo.individual ? 'individual ' : '';
+    var todoStyle = styles.title;
+    if(todo.priority === "C"){
+      todoStyle = styles.titleCritical;
+    }
+    if(todo.priority === "H"){
+      todoStyle = styles.titleHigh;
+    }
+    if(todo.priority === "M"){
+      todoStyle = styles.titleMedium;
+    }
+    var dueDateStyle = styles.dueDate;
+    console.log("Current Date: " + new Date());
+    if(todo.dueDate < new Date()) {
+      dueDateStyle = styles.dueDatePast;
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.title}> {todo.name} </Text>
+        <Text style={todoStyle}> {todo.name} </Text>
         <View style={styles.info}>
           <Text style={styles.individual}> {individual} </Text>
-          <Text style={styles.dueDate}> {dueDate} </Text>
+          <Text style={dueDateStyle}> {dueDate} </Text>
           <People people={todo.whoAreDone} />
         </View>
       </View>
