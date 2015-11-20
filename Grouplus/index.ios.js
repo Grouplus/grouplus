b93b2b3639ee50bcac1ddc7b61c0af0df9ff7e89
Ke Er Xiong <krx.sky@gmail.com>
2015-11-19 22:05:45 -0800

    can receive notification in background now

--- Grouplus/index.ios.js
+++ Grouplus/index.ios.js
@@ -4,8 +4,30 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  PushNotificationIOS,
 } = React;
 
 var Nav = require('./app/components/Nav');
-
+PushNotificationIOS.requestPermissions();
+var registerInstallation = function(data) {
+  var url = "https://api.parse.com";
+  url += "/1/installations";
+  fetch(url, {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'X-Parse-Application-Id': '***REMOVED***',
+      'X-Parse-REST-API-Key': '***REMOVED***',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+};
+PushNotificationIOS.addEventListener('register', function(token){
+  registerInstallation({
+    "deviceType": "ios",
+    "deviceToken": token,
+    "channels": ["global"]
+  })
+});
 AppRegistry.registerComponent('Grouplus', () => Nav);
